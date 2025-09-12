3c0fe172d50026e5dbe70b2072dad22df4554264
Andrew Chon <85811648+AndrewwChon@users.noreply.github.com>
2025-09-12 00:47:14 -0400

    Padframe added; 0912 Top-Level Sch/Sym; 0831 Top Level DRC/LVS Clean

---
+++ designs/libs/top_level/top_level_20250831/top_level_nosc_20250831/run_lvs.tcl
@@ -0,0 +1,19 @@
+# Tcl script to run LVS on top_level_nosc_20250831
+
+if {[catch {set PDK_ROOT $::env(PDK_ROOT)}]} {set PDK_ROOT /usr/share/pdk}
+if {[catch {set PDK $::env(PDK)}]} {set PDK gf180mcuD}
+
+set pdklib ${PDK_ROOT}/${PDK}
+set techlibs ${pdklib}/libs.tech
+set reflibs ${pdklib}/libs.ref
+
+set setupfile gf180mcuD_setup.tcl
+set sclib ${reflibs}/gf180mcu_fd_sc_mcu9t5v0/spice/gf180mcu_fd_sc_mcu9t5v0.spice
+
+set circuit1 [readnet spice top_level_nosc_20250831_layout.spice]
+set circuit2 [readnet spice $sclib]
+
+readnet spice top_level_nosc_20250831.spice $circuit2
+
+lvs "$circuit1 top_level_nosc_20250831" "$circuit2 top_level_nosc_20250831" \
+        $setupfile top_level_nosc_20250831_comp.out
